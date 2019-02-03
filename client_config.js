f2950a0d57a280e003fe0a5534c0c76791dbfc81
Ronen <RonenNess@gmail.com>
2019-02-04 00:50:37 +0200

    ..

--- client_config.js
+++ client_config.js
@@ -3,4 +3,14 @@ window._config = {
 
     // if true, will use cookies and localstorage instead of server to store data.
     use_browser_storage: true,
+
+    // max cards we allow on all columns
+    max_cards: 500,
+
+    // max description length of a card
+    max_card_description_length: 15000,
+
+    // play this alarm sound when deadline expires (url or null to disable)
+    play_alarm_sound: "alarm.wav",
+
 }
